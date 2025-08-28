e9d6d09bcad2ea313cdec7f95d57bc08dc56e2d2
Sonu Kumar <kumarsonu448866@gmail.com>
2025-08-28 10:36:43 +0530

    fixed

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [],
+    origin: "http://localhost:3000",
     methods: ["GET", "PUT", "POST", "DELETE"],
     credentials: true,
   })
